a19795a8a54472c6cff3fd70a798f5829ff24b9e
Cristian Castro <castro.cristiank@gmail.com>
2023-05-23 11:19:49 -0500

    Se crea el componente backend del proyecto. La estructura del proyecto es construido con Spring Initializr y Maven.

---
+++ backend/src/main/java/com/sergioarboleda/CarStore/CarStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sergioarboleda.CarStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sergioarboleda/CarStore/CarStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioarboleda.CarStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
